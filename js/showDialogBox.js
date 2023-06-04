840ba22162cc1a3ab0250c265f62011b292e8d76
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-06-04 12:30:19 -0600

    Set modal dialog to default behavior

--- js/showDialogBox.js
+++ js/showDialogBox.js
@@ -1,16 +1,1 @@
-const dialogContainer = document.querySelector(".dialog-boxes");
-const dialogBoxes = document.querySelectorAll("dialog");
-
 const createBookDialog = document.querySelector("#create-book-dialog");
-
-function CheckOpenedDialog() {
-  dialogBoxes.forEach((dialogBox) => {
-    if (dialogBox.attributes.open) {
-      dialogContainer.classList.add("dialog-shown");
-    } else {
-      dialogContainer.classList.remove("dialog-shown");
-    }
-  });
-}
-
-CheckOpenedDialog();
