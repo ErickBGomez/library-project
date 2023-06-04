7b70c3ac2211493aa281acc7cc2f676dad0d1f90
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-06-04 12:47:36 -0600

    Add open dialog animation

--- js/showDialogBox.js
+++ js/showDialogBox.js
@@ -1,1 +1,10 @@
 const createBookDialog = document.querySelector("#create-book-dialog");
+const createBookButton = document.querySelector(".create-book-button");
+
+function OpenDialogBox(dialog) {
+  dialog.showModal();
+}
+
+createBookButton.addEventListener("click", () =>
+  OpenDialogBox(createBookDialog)
+);
