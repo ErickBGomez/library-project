4ffda6492da492a1dec25132250dc92062857ff5
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-06-04 13:02:13 -0600

    Add closing dialog animation

--- js/showDialogBox.js
+++ js/showDialogBox.js
@@ -1,10 +1,27 @@
 const createBookDialog = document.querySelector("#create-book-dialog");
 const createBookButton = document.querySelector(".create-book-button");
+const cancelButton = document.querySelector(".cancel-button");
 
 function OpenDialogBox(dialog) {
+  dialog.classList.remove("close-dialog");
+  dialog.classList.add("open-dialog");
+
   dialog.showModal();
+}
+
+function CloseDialogBox(dialog) {
+  dialog.classList.remove("open-dialog");
+  dialog.classList.add("close-dialog");
+
+  // Wait until animation has finished
+  // And only close when dialog is at closing state
+  dialog.addEventListener("animationend", (e) => {
+    if (e.target.classList.value === "close-dialog") e.target.close();
+  });
 }
 
 createBookButton.addEventListener("click", () =>
   OpenDialogBox(createBookDialog)
 );
+
+cancelButton.addEventListener("click", () => CloseDialogBox(createBookDialog));
